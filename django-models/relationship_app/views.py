cbff48556c17d06a628804954d6be3f09756f4ba
helooselassie <selassieheloo@gmail.com>
2024-11-08 21:29:02 +0000

    Implement advanced model relationships

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -32,29 +32,7 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-# relationship_app/views.py
-from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
 
-# Login view (using Django's built-in LoginView)
-class UserLoginView(LoginView):
-    template_name = 'login.html'
-
-# Logout view (using Django's built-in LogoutView)
-class UserLogoutView(LogoutView):
-    template_name = 'logout.html'
-
-# Register view (using Django's built-in UserCreationForm)
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')  # Redirect to login page after successful registration
-    else:
-        form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
 
 # relationship_app/views.py
 from django.contrib.auth.views import LoginView, LogoutView
