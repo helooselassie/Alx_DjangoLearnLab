89daf90686ae2c39323282c529b6ea117c4e75f5
helooselassie <selassieheloo@gmail.com>
2024-11-09 23:10:57 +0000

    Implement advanced model relationships

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,11 +1,12 @@
 # relationship_app/urls.py
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView  # Import list_books and LibraryDetailView
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', list_books, name='list_books'),  # URL for function-based view
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL for class-based view
 ]
+
 
 
 # relationship_app/urls.py
