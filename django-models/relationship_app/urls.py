923f0d74db446982b3253d8840ec176c73c6c9c7
helooselassie <selassieheloo@gmail.com>
2024-11-09 23:28:00 +0000

    Implement advanced model relationships

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -25,3 +25,12 @@ urlpatterns = [
     path('register/', views.register, name='register'),
 ]
 
+# relationship_app/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   path('admin-view/', views.admin_view, name='admin_view'),
+   path('librarian-view/', views.librarian_view, name='librarian_view'),
+   path('member-view/', views.member_view, name='member_view'),
+]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -88,3 +88,31 @@ class RegisterView(View):
             form.save()
             return redirect('login')  # Adjust redirect as needed
         return render(request, 'relationship_app/register.html', {'form': form})
+
+
+# relationship_app/views.py
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
