20ef14f76c17673d45c708557482874f9075415e
helooselassie <selassieheloo@gmail.com>
2024-11-09 23:43:50 +0000

    Implement advanced model relationships

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -43,3 +43,23 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+    
+
+# relationship_app/models.py
+
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
