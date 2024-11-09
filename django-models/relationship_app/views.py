16a4b35e3de20465ff237bc92467670d0b218de0
helooselassie <selassieheloo@gmail.com>
2024-11-09 06:45:33 +0000

    Implement advanced model relationships

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,17 +6,8 @@ from .models import Book
 def list_books(request):
     books = Book.objects.all()  # Query all books
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
 # relationship_app/views.py
-
-from django.views.generic import DetailView
-from .models import Library
-
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-
-
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, logout
 from django.shortcuts import render, redirect
