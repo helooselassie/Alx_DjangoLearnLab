d84a960d5ad987e16aaaf05644cd87a63bf66af8
helooselassie <selassieheloo@gmail.com>
2024-11-09 06:50:35 +0000

    Implement advanced model relationships

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -8,12 +8,19 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL pattern for the class-based view
 ]
 
-# urls.py
+# relationship_app/urls.py
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    # Login view with custom template
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    
+    # Logout view with custom template
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    
+    # Register view
     path('register/', views.register, name='register'),
 ]
+
