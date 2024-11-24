e1a57f6d40ba567b7e5c531b656802f72319c29b
helooselassie <selassieheloo@gmail.com>
2024-11-24 22:11:10 +0000

    Initial commit of Django project

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path
 from django.contrib import admin
 from django.urls import path, include  # Include function for app URLs
+from .views import BookList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Include the api app's URLs under the /api/ path
+    path('books/', BookList.as_view(), name='book-list'),
 ]
