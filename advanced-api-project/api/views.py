2122c2bb1538290b603bc2046cf1f2b76102d31e
helooselassie <selassieheloo@gmail.com>
2024-11-27 15:43:48 +0000

    Initial commit of advanced_api_project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -9,8 +9,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import AllowAny
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly,IsAuthenticated
 
 class BookListView(generics.ListAPIView):
        queryset = Book.objects.all()
