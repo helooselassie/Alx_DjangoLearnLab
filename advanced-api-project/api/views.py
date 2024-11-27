5e82a690e2e3b8937f070d4f6b5a359c33054a6c
helooselassie <selassieheloo@gmail.com>
2024-11-27 16:13:43 +0000

    Initial commit of advanced_api_project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -15,6 +15,8 @@ from rest_framework import viewsets
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters import rest_framework
 from .filters import BookFilter
+from rest_framework.filters import OrderingFilter
+
 
 class BookListView(generics.ListAPIView):
        queryset = Book.objects.all()
