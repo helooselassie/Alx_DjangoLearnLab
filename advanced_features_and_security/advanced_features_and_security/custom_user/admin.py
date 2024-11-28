0900772707d13f516e0d39c03387d33ffd8f1aa2
helooselassie <selassieheloo@gmail.com>
2024-11-28 12:34:20 +0000

    Initial commit of advanced_features_and_security

--- advanced_features_and_security/advanced_features_and_security/custom_user/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
-
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ['email', 'username', 'date_of_birth', 'is_staff']
-    
-admin.site.register(CustomUser, CustomUserAdmin)
