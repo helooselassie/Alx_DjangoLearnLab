46e8d27a65010e6224e404b7389288732fa0572a
helooselassie <selassieheloo@gmail.com>
2024-11-29 15:04:35 +0000

    Initial commit of advanced_features_and_security

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -60,3 +61,19 @@ class UserProfile(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view article"),
+            ("can_create", "Can create article"),
+            ("can_edit", "Can edit article"),
+            ("can_delete", "Can delete article"),
+        ]
+
+    def __str__(self):
+        return self.title
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+# views.py
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseForbidden
+from django.contrib.auth.decorators import login_required, permission_required
+from .models import Article
 
-# Create your views here.
+@login_required
+@permission_required('app_name.can_view', raise_exception=True)
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'article_list.html', {'articles': articles})
+
+@login_required
+@permission_required('app_name.can_create', raise_exception=True)
+def article_create(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        article = Article.objects.create(title=title, content=content, author=request.user)
+        return render(request, 'article_detail.html', {'article': article})
+    return render(request, 'article_form.html')
+
+@login_required
+@permission_required('app_name.can_edit', raise_exception=True)
+def article_edit(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    if request.method == "POST":
+        article.title = request.POST.get("title")
+        article.content = request.POST.get("content")
+        article.save()
+        return render(request, 'article_detail.html', {'article': article})
+    return render(request, 'article_form.html', {'article': article})
+
+@login_required
+@permission_required('app_name.can_delete', raise_exception=True)
+def article_delete(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    if request.method == "POST":
+        article.delete()
+        return render(request, 'article_list.html')
+    return render(request, 'article_confirm_delete.html', {'article': article})
