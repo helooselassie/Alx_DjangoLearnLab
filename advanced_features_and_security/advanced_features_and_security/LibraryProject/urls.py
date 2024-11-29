81dfeb36fc3083d55a5a94a7ec51f796137d1f0e
helooselassie <selassieheloo@gmail.com>
2024-11-29 14:52:01 +0000

    Initial commit of advanced_features_and_security

--- advanced_features_and_security/advanced_features_and_security/LibraryProject/urls.py
+++ advanced_features_and_security/advanced_features_and_security/LibraryProject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for advanced_features_and_security project.
+URL configuration for LibraryProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -19,4 +19,14 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+# LibraryProject/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('relationship_app/', include('relationship_app.urls')),  # Include relationship_app URLs
 ]
--- advanced_features_and_security/advanced_features_and_security/manage.py
+++ advanced_features_and_security/advanced_features_and_security/manage.py
@@ -2,11 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+print(os.environ.get('DJANGO_SETTINGS_MODULE'))
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'advanced_features_and_security.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
