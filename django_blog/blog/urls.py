c65c75116c119437aa7323593a4758e8a39aaac1
helooselassie <selassieheloo@gmail.com>
2024-12-05 17:40:43 +0000

    django blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -15,11 +15,11 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', views.register_view, name='register'),
     path('profile/', views.profile_view, name='profile'),
-    path('', PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('', PostListView.as_view(), name='post-list'), 
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  
+    path('post/new/', PostCreateView.as_view(), name='post-create'),  
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), 
 ]
 
 
