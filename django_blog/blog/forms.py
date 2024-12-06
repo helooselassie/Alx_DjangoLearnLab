32c367597fe4b8a0b7e9835c64006fedb7665628
helooselassie <selassieheloo@gmail.com>
2024-12-06 11:44:36 +0000

    django blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,24 +1,40 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Comment
-from taggit.forms import TagWidget  # If you're using django-taggit for tags
+from taggit.forms import TagWidget  # For handling tags if using django-taggit
 
+# Post creation and update form with a custom widget for tags
 class PostForm(forms.ModelForm):
+    # Define the tags field with TagWidget if using django-taggit
     tags = forms.CharField(
-        widget=TagWidget(),  # TagWidget is used to display tags input
+        widget=TagWidget(),  # Uses TagWidget to allow tag input
         required=False
     )
 
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags']  # Include tags in form fields
+        fields = ['title', 'content', 'tags']  # Include tags in the form fields
 
+# Comment form doesn't require a custom widget but could include one if needed
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
 
-# Optionally, you can create a widget for other custom uses:
+# Optional: Define a custom widget for any form field
 class CustomTagWidget(forms.TextInput):
-    # Define the custom widget behavior if needed
-    pass
+    template_name = 'custom_widget_template.html'  # Specify the template for the widget
+
+    def __init__(self, attrs=None):
+        if attrs is None:
+            attrs = {}
+        attrs.update({'class': 'custom-class'})  # Add custom classes or attributes
+        super().__init__(attrs)
+
+# Another example: using a custom widget for another form field
+class CustomContentWidget(forms.Textarea):
+    def __init__(self, attrs=None):
+        if attrs is None:
+            attrs = {}
+        attrs.update({'class': 'custom-textarea-class'})
+        super().__init__(attrs)
