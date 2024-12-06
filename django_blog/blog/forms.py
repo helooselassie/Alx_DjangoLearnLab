db5875ae503abe1783727f3c000f4fd5de0a9843
helooselassie <selassieheloo@gmail.com>
2024-12-06 11:43:21 +0000

    django blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,24 +1,24 @@
-# blog/forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from .models import Comment
-from .models import Post
+from .models import Post, Comment
+from taggit.forms import TagWidget  # If you're using django-taggit for tags
 
 class PostForm(forms.ModelForm):
+    tags = forms.CharField(
+        widget=TagWidget(),  # TagWidget is used to display tags input
+        required=False
+    )
+
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags']  # Include the tags field
-
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-
+        fields = ['title', 'content', 'tags']  # Include tags in form fields
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
+# Optionally, you can create a widget for other custom uses:
+class CustomTagWidget(forms.TextInput):
+    # Define the custom widget behavior if needed
+    pass
