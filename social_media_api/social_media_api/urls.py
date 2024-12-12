7ca38b2eec77b0f9ca9e71e3340db0b161251a34
helooselassie <selassieheloo@gmail.com>
2024-12-12 14:07:41 +0000

    Initial commit of Social Media API project

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),  
     path('posts/', include('posts.urls')),  
+    path('api/', include('posts.urls')),
 ]
 
