54a2762ccf38c830eb098ecc75d20854d6ccbb33
helooselassie <selassieheloo@gmail.com>
2024-12-15 15:43:43 +0000

    Initial commit of Social Media API project

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -79,10 +79,20 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alx_social_media',
+        'USER': 'postgres', 
+        'PASSWORD': 'sela123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
