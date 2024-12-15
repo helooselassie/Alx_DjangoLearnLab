e83685af06183a1c0b92ae88c3f218e0d2943276
helooselassie <selassieheloo@gmail.com>
2024-12-15 12:24:26 +0000

    Initial commit of Social Media API project

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -141,3 +141,11 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ),
+}    
